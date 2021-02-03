e9edcfe1990fd30af42b0d50bdbb1410b6d43b91
Chukwuemekaajah <talk2ajah@gmail.com>
2021-02-03 16:17:26 +0100

    Setup tests for auth

--- buycoins_python/components/utilities.py
+++ buycoins_python/components/utilities.py
@@ -1,7 +1,35 @@
 import requests
 auth = None
 
-def make_request(url, method='post', headers=auth, params={}, data={}):
-    print("making a request now")
-    print("auth is", auth)
-    return "hello world"
+def make_request(url, method='post', headers={'Content-type':'application/json', 'Accepts':'application/json'}, params={}, data={}):
+    response = requests.request(method, url, headers=headers, auth=auth, data=data, params=params)
+    return response
+
+
+def _get_messages(errors):
+    return list(map(lambda error: error.message, errors))
+
+def _get_fields(errors):
+    return list(map(lambda error: (error.path and error.path.join) or "", errors))
+
+def _create_error_response(errors):
+    messages = _get_messages(errors)
+    fields = _get_fields(errors)
+    response = []
+    for i in range(len(messages)):
+        response.append({"reason":messages[i], "field": fields[i] })
+    return response
+
+def parse_response(response):
+    jsonResponse = response.json()
+    if(jsonResponse.errors):
+        return {
+            "status": "failure",
+            "errors": _create_error_response(jsonResponse.errors),
+            "raw": jsonResponse.errors
+        }
+    else:
+        return {
+            "status": "success",
+            "data": response.data
+        }
