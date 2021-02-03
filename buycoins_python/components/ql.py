e3e1f2f9fc8a1277fd726eabd20157e3cce8ac3a
Chukwuemekaajah <talk2ajah@gmail.com>
2021-02-03 23:08:48 +0100

    Implement graphql request body creator

--- buycoins_python/components/ql.py
+++ buycoins_python/components/ql.py
@@ -1,9 +1,3 @@
-
-AUTH = None
-API_URL = "https://backend.buycoins.tech/api/graphql"
-HEADERS = {'Content-type':'application/json', 'Accepts':'application/json'}
-
-
 def create_request_body(fields):
     operations = ["query", "mutation"]
 
@@ -56,32 +50,29 @@ def parse_fields(fields, fields_array=[]):
 
     return ",".join(fields_array)
 
+query = {
+    "operation": "mutation",
+    "command": "getPrices",
+    "args": {"id":"chuks", "name":"emeka"},
+    "fields":[
+        {"field":"name", "args":{"id":"chuks","name":"meat"}, "fields":[{"field":"first"}]},
+        {"field":"age"}
+    ]
+}
+# mutation {
+#   createAddress(cryptocurrency: bitcin) {
+#     cryptocurrency
+#     address
+#   }
+# }
+ql = {
+    "operation": "mutation",
+    "command": "   ",
+    "args": {"cryptocurrency":"bitcoin"},
+    "fields":[
+        {"field":"cryptocurrency"},
+        {"field":"address"}
+    ]
+}
 
-def _get_messages(errors):
-    return list(map(lambda error: error.get("message", ""), errors))
-
-def _get_fields(errors):
-    return list(map(lambda error: (error.get("path", []) and ".".join(error.get("path",[]))) or "", errors))
-
-def _create_error_response(errors):
-    messages = _get_messages(errors)
-    fields = _get_fields(errors)
-    response = []
-    for i in range(len(messages)):
-        response.append({"reason":messages[i], "field": fields[i] })
-    return response
-
-def parse_response(response):
-    jsonResponse = response.json()
-    
-    if(jsonResponse.get("errors")):
-        return {
-            "status": "failure",
-            "errors": _create_error_response(jsonResponse.get("errors", [])),
-            "raw": jsonResponse.get('errors', [])
-        }
-    else:
-        return {
-            "status": "success",
-            "data":jsonResponse.get("data",{})
-        }
+print(create_request_body(ql))
