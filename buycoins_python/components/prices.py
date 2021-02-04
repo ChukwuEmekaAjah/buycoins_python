b36920a65976e43344c3f1e4305fb7af1566fd15
Chukwuemekaajah <talk2ajah@gmail.com>
2021-02-04 23:12:33 +0100

    Add documentation for prices module and tests discovery init file

--- buycoins_python/components/prices.py
+++ buycoins_python/components/prices.py
@@ -4,10 +4,34 @@ from . import utilities
 
 
 def list(fields:list = []):
-    """
-        Retrieve a list of cryptocurrency prices
+    """Retrieve a list of cryptocurrency prices
 
-        If fields parameter is empty, it defaults to retrieving all the fields
+    Returns the list of cryptocurrency prices being traded on the Buycoins platform. It utilizes the `getPrices` query on the API.
+
+    Args:
+        fields (list):
+            The fields you want returned by the graphql query. It defaults to all the query fields of the getPrices query if this argument is absent or empty. It is a list of dictionaries. 
+            An example field dict is represented as:
+
+            {"field": "estimatedFee"}
+
+            Default fields are:
+            [{"field": "id"}, {"field": "minSell"}]
+    
+    Returns:
+        A dict mapping containing a status key which can either be one of: "failure" or "success" and an errors/data key  depending on the status of the request. For example:
+
+        {"status":"success",
+         "data":{
+            "postListOrder":{
+                "id":"afWGFdfa823ladfadfja",
+                "status":"open"
+            }
+         }
+        }
+
+    Raises:
+        Exception: Only raised if fields parameter has an item dict without the field property.
     """
     # add validation for fields
 
