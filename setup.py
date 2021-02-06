e480ad0e8a29d0ee7a442421ecf6cb2d024b43aa
Chukwuemekaajah <talk2ajah@gmail.com>
2021-02-06 16:36:42 +0100

    Add long description in setup file

--- setup.py
+++ setup.py
@@ -1,2 +1,8 @@
 from setuptools import setup
-setup(name='buycoins_python', version='0.1', description='Buycoins API Python client', url='http://github.com/storborg/funniest', author='Chukwuemeka Ajah', author_email='talk2ajah@gmail.com', license='MIT', packages=['buycoins_python'], install_requires=['graphene', 'requests'], zip_safe=False)
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='buycoins_python', version='0.1', description='Buycoins API Python client', url='http://github.com/storborg/funniest', author='Chukwuemeka Ajah', author_email='talk2ajah@gmail.com', license='MIT', packages=['buycoins_python'], install_requires=['graphene', 'requests'], zip_safe=False, long_description=long_description, long_description_content_type='text/markdown')
