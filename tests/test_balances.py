68d7913bda81b1149af7b866aba0df1d05cf672f
Chukwuemekaajah <talk2ajah@gmail.com>
2021-02-06 14:03:55 +0100

    Update test for balances get

--- tests/test_balances.py
+++ tests/test_balances.py
@@ -21,6 +21,15 @@ class TestBalancesMethods(unittest.TestCase):
             Balances.list([{"field":"cryptocrrency"}, {"name":"chuks"}])
         except Exception as e:
             self.assertEqual(str(e), "Fields contains a node dict without a 'field' property.")
+
+    def test_invalid_get_parameter(self):
+        """
+            Should throw an exception for invalid single cryptocurrency retrieval parameter
+        """
+        try:
+            Balances.get("    ")
+        except Exception as e:
+            self.assertEqual(str(e), "cryptocurrency argument must be a valid string identifier.")
     
     @patch('buycoins_python.Balances.requests.post')  # Mock 'requests' module 'post' method.
     def test_failed_Balances_retrieval(self, mock_post):
@@ -37,7 +46,21 @@ class TestBalancesMethods(unittest.TestCase):
         self.assertEqual(response["errors"][0]["reason"], "Field 'cryptocurrenc' doesn't exist on type 'Account'")
 
     @patch('buycoins_python.Balances.requests.post')  # Mock 'requests' module 'post' method.
-    def test_successful_Balances_retrieval(self, mock_post):
+    def test_successful_Balances_get(self, mock_post):
+        """
+            Should return a success status for successful single balance retrieval
+        """
+        mock_post.return_value = MockResponse({"data":{"getBalances":[{"id":"QWNjb3VudC0=","cryptocurrency":"usd_tether","confirmedBalance":"0.0"}]}}, 200)
+        
+        Auth.setup("chuks", "emeka")
+        response = Balances.get("usd_tether")
+        
+        self.assertEqual(response['status'], "success")
+        self.assertEqual(response["data"]["getBalances"][0]["id"], "QWNjb3VudC0=")
+        self.assertEqual(response["data"]["getBalances"][0]["cryptocurrency"], "usd_tether")
+
+    @patch('buycoins_python.Balances.requests.post')  # Mock 'requests' module 'post' method.
+    def test_successful_Balances_list(self, mock_post):
         """
             Should return a success status for successful Balances retrieval
         """
