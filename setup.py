8b53a3cf50ecfb63ef272051703f98034e0901be
Chukwuemekaajah <talk2ajah@gmail.com>
2021-02-06 19:10:29 +0100

    Remove github workflow file

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
 setup(name='buycoins_python', 
     version='0.1', 
     description='Buycoins API Python client', 
-    url='http://github.com/storborg/funniest', 
+    url='https://github.com/ChukwuEmekaAjah/buycoins_python', 
     author='Chukwuemeka Ajah', 
     author_email='talk2ajah@gmail.com', 
     license='MIT', 
     packages=['buycoins_python'], 
     install_requires=['requests'], 
-    zip_safe=False, 
+    zip_safe=True, 
     long_description=long_description, 
     long_description_content_type='text/markdown'
 )
